22ceb8efe9b308654ce638117381b1cade94e0bb
Munyaradzi Muchenje <munyaradzi.muchenje@scalexa.com>
2023-03-01 08:31:51 +0200

    init commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar";
+import Slider from "../components/Slider";
+
+const HomePage = () => {
+    return (
+        <div>
+            <NavBar />
+            <Slider />
+        </div>    
+    )
+}
+
+export default HomePage;
