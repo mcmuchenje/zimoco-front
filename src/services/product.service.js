78ee38f8fd5046d25bcccd77db40db49bb81679d
Munyaradzi Muchenje <munyaradzi.muchenje@scalexa.com>
2023-03-07 07:55:48 +0200

    added changes

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "../auth-header";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "http://45.77.53.86:8080/api";
 
 const getAll = async () => {
   return await axios.get(API_URL + "/products", { headers: authHeader() });
